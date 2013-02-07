7e11a053743efe115d5066e4040e7204ecdbda13
sunteya <sunteya@gmail.com>
2013-02-07 22:56:36 +0800

    support blacklist pac

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,11 +1,17 @@
 class MainController < ApplicationController
-  before_filter :authenticate_user!, only: [ :root, :whitelist ]
+  before_filter :authenticate_user!, except: :grant
   
   def root
     redirect_to current_user
   end
   
   def whitelist
+    expires_in 1.hour, public: true
+    @user = current_user
+  end
+  
+  def blacklist
+    expires_in 1.hour, public: true
     @user = current_user
   end
   
