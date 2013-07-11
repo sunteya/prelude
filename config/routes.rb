69529f9496894daafef62a2d55ce3cca07812163
sunteya <sunteya@gmail.com>
2013-07-11 15:25:37 +0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Prelude::Application.routes.draw do
   
   get "/whitelist" => "main#whitelist"
   get "/blacklist" => "main#blacklist"
-  match "/grant" => "main#grant"
+  post "/grant" => "main#grant"
   
 end
