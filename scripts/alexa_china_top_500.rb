06cc5b80dba81408f9583283a4b8ac46f6d185b4
sunteya <sunteya@gmail.com>
2015-08-25 12:35:56 +0800

    add alexa china top fetch script

---
+++ scripts/alexa_china_top_500.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require "bundler/inline"
+
+gemfile do
+  source 'https://rubygems.org'
+  gem "mechanize"
+  gem "pry"
+end
+
+agent = Mechanize.new
+
+sites = []
+20.times do |n|
+  pagination = ";#{n}" if n > 0
+  url = "http://www.alexa.com/topsites/countries#{pagination}/CN"
+  agent.get(url) do |page|
+    sites += page.search(".site-listing .desc-paragraph a").map { |a| a[:href].sub('/siteinfo/', '') }
+  end
+end
+
+puts "# Alieax Top 500 Sites in China without '.cn' (#{Time.now.strftime('%Y%m%d')})"
+puts sites.find_all { |site| !site.end_with?(".cn") }
