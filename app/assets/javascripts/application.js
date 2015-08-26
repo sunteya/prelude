ac14c45849d3a234ff34c6119b2c7689f5f63b16
sunteya <sunteya@gmail.com>
2015-08-26 14:08:23 +0800

    morris report work with bootstrap v4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,39 +2,33 @@
 //= require jquery-ujs
 //= require bootstrap
 
-//  raphael
-//  morris
+//= require  raphael
+//= require  morris
 
-// function numberToHumanSize(bytes) {
-// 	if (bytes == null) {
-// 		return "-";
-// 	}
+function numberToHumanSize(bytes) {
+	if (bytes == null) {
+		return "-";
+	}
 
-// 	var precision = 2;
-// 	var kilobyte = 1024;
-// 	var megabyte = kilobyte * 1024;
-// 	var gigabyte = megabyte * 1024;
-// 	var terabyte = gigabyte * 1024;
+	var precision = 2;
+	var kilobyte = 1024;
+	var megabyte = kilobyte * 1024;
+	var gigabyte = megabyte * 1024;
+	var terabyte = gigabyte * 1024;
 
-// 	if ((bytes >= kilobyte) && (bytes < megabyte)) {
-// 		return (bytes / kilobyte).toFixed(precision) + ' KB';
+	if ((bytes >= kilobyte) && (bytes < megabyte)) {
+		return (bytes / kilobyte).toFixed(precision) + ' KB';
 
-// 	} else if ((bytes >= megabyte) && (bytes < gigabyte)) {
-// 		return (bytes / megabyte).toFixed(precision) + ' MB';
+	} else if ((bytes >= megabyte) && (bytes < gigabyte)) {
+		return (bytes / megabyte).toFixed(precision) + ' MB';
 
-// 	} else if ((bytes >= gigabyte) && (bytes < terabyte)) {
-// 		return (bytes / gigabyte).toFixed(precision) + ' GB';
+	} else if ((bytes >= gigabyte) && (bytes < terabyte)) {
+		return (bytes / gigabyte).toFixed(precision) + ' GB';
 
-// 	} else if (bytes >= terabyte) {
-// 		return (bytes / terabyte).toFixed(precision) + ' TB';
+	} else if (bytes >= terabyte) {
+		return (bytes / terabyte).toFixed(precision) + ' TB';
 
-// 	} else {
-// 		return bytes + ' B';
-// 	}
-// }
-
-// $(function() {
-// 	$('body').on('touchstart.dropdown', '.dropdown-menu', function (e) {
-// 		e.stopPropagation();
-// 	});
-// })
+	} else {
+		return bytes + ' B';
+	}
+}
