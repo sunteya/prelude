4ac152f10e4bb832b8f22204cc107d3900dcfdf7
sunteya <sunteya@gmail.com>
2015-08-26 12:02:42 +0800

    bootstrap v4 layout and table

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,51 +1,40 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
-//= require jquery_ujs
+//= require jquery-ujs
 //= require bootstrap
-//= require raphael
-//= require morris
 
-function numberToHumanSize(bytes) {
-	if (bytes == null) {
-		return "-";
-	}
-	
-	var precision = 2;
-	var kilobyte = 1024;
-	var megabyte = kilobyte * 1024;
-	var gigabyte = megabyte * 1024;
-	var terabyte = gigabyte * 1024;
+//  raphael
+//  morris
 
-	if ((bytes >= kilobyte) && (bytes < megabyte)) {
-		return (bytes / kilobyte).toFixed(precision) + ' KB';
+// function numberToHumanSize(bytes) {
+// 	if (bytes == null) {
+// 		return "-";
+// 	}
 
-	} else if ((bytes >= megabyte) && (bytes < gigabyte)) {
-		return (bytes / megabyte).toFixed(precision) + ' MB';
+// 	var precision = 2;
+// 	var kilobyte = 1024;
+// 	var megabyte = kilobyte * 1024;
+// 	var gigabyte = megabyte * 1024;
+// 	var terabyte = gigabyte * 1024;
 
-	} else if ((bytes >= gigabyte) && (bytes < terabyte)) {
-		return (bytes / gigabyte).toFixed(precision) + ' GB';
+// 	if ((bytes >= kilobyte) && (bytes < megabyte)) {
+// 		return (bytes / kilobyte).toFixed(precision) + ' KB';
 
-	} else if (bytes >= terabyte) {
-		return (bytes / terabyte).toFixed(precision) + ' TB';
+// 	} else if ((bytes >= megabyte) && (bytes < gigabyte)) {
+// 		return (bytes / megabyte).toFixed(precision) + ' MB';
 
-	} else {
-		return bytes + ' B';
-	}
-}
+// 	} else if ((bytes >= gigabyte) && (bytes < terabyte)) {
+// 		return (bytes / gigabyte).toFixed(precision) + ' GB';
 
-$(function() {
-	$('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { 
-		e.stopPropagation();
-	});
-})
+// 	} else if (bytes >= terabyte) {
+// 		return (bytes / terabyte).toFixed(precision) + ' TB';
+
+// 	} else {
+// 		return bytes + ' B';
+// 	}
+// }
+
+// $(function() {
+// 	$('body').on('touchstart.dropdown', '.dropdown-menu', function (e) {
+// 		e.stopPropagation();
+// 	});
+// })
